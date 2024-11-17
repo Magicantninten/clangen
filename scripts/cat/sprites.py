351cd03846db67004ebe30f6e31e08ef3ff65d6d
magicantninten <marsh.je.silvester@gmail.com>
2024-11-17 16:38:02 -0600

    print("For myself")

--- scripts/cat/sprites.py
+++ scripts/cat/sprites.py
@@ -226,7 +226,7 @@ class Sprites:
             for col, mask in enumerate(masks):
                 self.make_group('tortiepatchesmasks', (col, row), f"tortiemask{mask}")
 
-        # Define skin colors 
+        # Define skin colors
         skin_colors = [
             ['BLACK', 'RED', 'PINK', 'DARKBROWN', 'BROWN', 'LIGHTBROWN'],
             ['DARK', 'DARKGREY', 'GREY', 'DARKSALMON', 'SALMON', 'PEACH'],
@@ -261,7 +261,7 @@ class Sprites:
             ["LEFTEAR", "RIGHTEAR", "NOTAIL", "NOLEFTEAR", "NORIGHTEAR", "NOEAR", "HALFTAIL", "NOPAW"]
         ]
 
-        # scars 
+        # scars
         for row, scars in enumerate(scars_data):
             for col, scar in enumerate(scars):
                 self.make_group('scars', (col, row), f'scars{scar}')
@@ -316,7 +316,7 @@ class Sprites:
         #dryherbs
         for row, dry in enumerate(dryherbs_data):
             for col, dryherbs in enumerate(dry):
-                self.make_group('medcatherbs', (col, 3), f'acc_herbs{dryherbs}')     
+                self.make_group('medcatherbs', (col, 3), f'acc_herbs{dryherbs}'),
         # wild
         for row, wilds in enumerate(wild_data):
             for col, wild in enumerate(wilds):
